5223cb9bc2b4919034506995f668a4690c91bc88
Rit-002 <64945109+Rit-01@users.noreply.github.com>
2021-03-29 00:48:45 +0530

    djangoecom

--- Ecom/settings.py
+++ Ecom/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,9 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = "/images/downloads/"
 MEDIA_ROOT = BASE_DIR
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]

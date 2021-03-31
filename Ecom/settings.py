eaa1faae6bcc84f149fbc455e19e6850a7476c0c
Rit-01 <64945109+Rit-01@users.noreply.github.com>
2021-03-31 18:14:48 +0530

    Update settings.py

--- Ecom/settings.py
+++ Ecom/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,8 +121,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = "/images/downloads/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = BASE_DIR
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]

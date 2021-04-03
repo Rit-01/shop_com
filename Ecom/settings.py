05fd4b6a3f8497ea83ca79c83e6b24aa26b0f214
Rit-01 <64945109+Rit-01@users.noreply.github.com>
2021-04-04 04:11:13 +0530

    allow

--- Ecom/settings.py
+++ Ecom/settings.py
@@ -120,6 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
 MEDIA_URL = "/images/downloads/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = BASE_DIR
